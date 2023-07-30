8410c821152640dccf2b3e5c5141c0d3d4645cf4
Muthuvignesh <101090514+Muthuvignesh25@users.noreply.github.com>
2023-07-30 17:15:58 +0530

    cicd

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,2 +1,12 @@
-def git(){
+def git(repo){
+  git 'https://github.com/intelliqittrainings/${repo}.git'
+}
+def maven(){
+  sh 'mvn package'
+}
+def deploy(workspace,ip,contextpath){
+  sh 'scp /var/lib/jenkins/workspace/{workspace}/webapp/target/webapp.war ubuntu@{ip}:/var/lib/tomcat9/webapps/{contextpath}.war'
+}
+def test(workspace){
+  sh 'java -jar /var/lib/jenkins/workspace/{workspace}/testing.jar'
 }
