dd1e89b20ed83da3398998818b07a6364da7c372
Muthuvignesh <101090514+Muthuvignesh25@users.noreply.github.com>
2023-07-30 17:39:23 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,11 +2,11 @@ def gitv(repo){
   git "https://github.com/intelliqittrainings/${repo}"
 }
 def mavenv(){
-  sh 'mvn package'
+  sh "mvn package"
 }
 def deployv(workspace,ip,contextpath){
-  sh 'scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war'
+  sh "scp /var/lib/jenkins/workspace/${workspace}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
 def testv(workspace){
-  sh 'java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar'
+  sh "java -jar /var/lib/jenkins/workspace/${workspace}/testing.jar"
 }
